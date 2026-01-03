a90279d48617808aa2a00c9614b9eac409895070
akj392 <akj392@gmail.com>
2026-01-04 01:02:07 +0530

    3m accesstoken

--- helpers/tokenHelper.js
+++ helpers/tokenHelper.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (user) =>
-    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3m" });
 
 const generateRefreshToken = (user) =>
     jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
